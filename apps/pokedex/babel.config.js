6c5307b253044509b391baee51c0fc30bfc5725d
aintz <aint.iglesias@gmail.com>
2025-10-06 19:02:12 +0200

    Fix Babel config

--- apps/pokedex/babel.config.js
+++ apps/pokedex/babel.config.js
@@ -1,10 +1,12 @@
-export const presets = [
-	["@babel/preset-env", { targets: { node: "current" } }],
-	[
-		"@babel/preset-react",
-		{
-			runtime: "automatic",
-		},
+module.exports = {
+	presets: [
+		["@babel/preset-env", { targets: { node: "current" } }],
+		[
+			"@babel/preset-react",
+			{
+				runtime: "automatic",
+			},
+		],
+		"@babel/preset-typescript",
 	],
-	"@babel/preset-typescript",
-];
+};
